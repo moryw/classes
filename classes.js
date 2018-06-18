cb830c929f864e5e27deb9be4206a3fac8ed13b2
moryw <matt@skitskat.xyz>
2018-06-18 16:16:26 +0700

    Add cc sample code

---
+++ classes.js
@@ -0,0 +1,18 @@
+//sampe code from cc
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }
+
+  incrementBehavior() {
+    this._behavior ++;
+  }
+}
