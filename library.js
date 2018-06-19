0ad7b426be3ff58f1d104c44c48a9f5bce5397a4
moryw <matt@skitskat.xyz>
2018-06-19 19:22:56 +0700

    Add three methods to the Media class.

--- library.js
+++ library.js
@@ -19,4 +19,51 @@ class Media {
     return this._ratings;
   }
 
+  toggleCheckOutStatus(newStatus) {
+    return this._isCheckedOut = newStatus;
+  }
+
+  getAverageRating() {
+    let sum = 0;
+    for (var i = 0; i < this._ratings.length; i++) {
+      sum += this._ratings[i]
+    }
+    let avg = sum / this._ratings.length;
+    console.log(`The average rating is ${avg}.`);
+  }
+
+  addRating(newRating) {
+    return this._ratings.push(newRating);
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//end
