f572c824dac1b983ad5c82c4a0394cfcf05c8fff
moryw <matt@skitskat.xyz>
2018-06-19 17:38:31 +0700

    Practice with super class and sub class and instances.

--- classes.js
+++ classes.js
@@ -30,16 +30,83 @@ class Surgeon {
   constructor(name, department) {
     this._name = name;
     this._department = department;
+    this._remainingVacationDays = 20;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    console.log(`${daysOff} day/s off were taken.`);
+    this._remainingVacationDays = this._remainingVacationDays - daysOff
+  }
+
 }
 
 const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
 const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
 
-console.log(surgeonCurry._name);
-console.log(surgeonDurant._name);
+// console.log(surgeonCurry.name);
+// surgeonCurry.takeVacationDays(3);
+// console.log(surgeonCurry.remainingVacationDays);
 
-console.log(surgeonCurry._department);
+//hospital employee parent/superclass class practice
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return `There are ${this._remainingVacationDays} vacation days remaining.`;
+  }
+
+  takeVacationDays(daysOff) {
+    console.log(`${daysOff} day/s off were taken.`);
+    this._remainingVacationDays = this._remainingVacationDays - daysOff;
+  }
+
+}
+
+//nurse child/sub class that extends the hospital employee
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+
+  get certifications() {
+    return this._certifications;
+  }
+
+  addCertification(newCertification) {
+    this._certifications.push(newCertification);
+  }
+
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+
+console.log(nurseOlynyk.certifications);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+
+
+
+
 
 
 
