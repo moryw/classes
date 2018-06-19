2fbc7355f0b9af91937bb45ea718e5b305ba0d5c
moryw <matt@skitskat.xyz>
2018-06-19 19:36:23 +0700

    Add getters to book sub class for author and pages.

--- library.js
+++ library.js
@@ -45,6 +45,15 @@ class Book extends Media {
     this._author = author;
     this._pages = pages;
   }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+
 }
 
 const testBook = new Book('Test Title', 'Test Author', 100);
